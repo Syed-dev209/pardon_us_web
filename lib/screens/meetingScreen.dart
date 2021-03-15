67130590d0e6be151b1eae9b83f4a1f3df2b995e
Syed-dev209 <60479224+Syed-dev209@users.noreply.github.com>
2021-03-15 02:12:26 -0700

    video calling ne marwa li

--- lib/screens/meetingScreen.dart
+++ lib/screens/meetingScreen.dart
@@ -1,4 +1,14 @@
+import 'dart:async';
+import 'dart:html' as html;
+import 'package:agora_rtc_engine/rtc_engine.dart';
 import 'package:flutter/material.dart';
+import 'package:pardon_us/models/userDeatils.dart';
+//import 'package:pardon_us/screens/CallPage2.dart';
+import 'package:pardon_us/screens/callPage.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:provider/provider.dart';
+import 'utils/settings.dart';
+
 
 class MeetingScreen extends StatefulWidget {
   @override
@@ -6,46 +16,134 @@ class MeetingScreen extends StatefulWidget {
 }
 
 class _MeetingScreenState extends State<MeetingScreen> {
+  final _channelController = TextEditingController();
+  final _passwordController = TextEditingController();
+  bool _validateError = false;
+  ClientRole _role;
+
+  @override
+  void dispose() {
+    // dispose input controller
+    _channelController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Padding(
-        padding:  EdgeInsets.symmetric(horizontal: 12.0,vertical: 13.0),
+        padding: EdgeInsets.symmetric(horizontal: 12.0, vertical: 13.0),
         child: Column(
           children: [
             TextFormField(
-              decoration:  InputDecoration(
+              controller: _channelController,
+              decoration: InputDecoration(
                   labelText: 'Meeting Code',
                   border: OutlineInputBorder(),
                   hintText: 'Enter meeting code here...',
-                  contentPadding: EdgeInsets.symmetric(horizontal: 5.0, vertical:2.0)
-              ),
+                  contentPadding:
+                  EdgeInsets.symmetric(horizontal: 5.0, vertical: 2.0)),
             ),
-            SizedBox(height: 20.0,),
+            SizedBox(height: 4.0,),
+            TextFormField(
+              controller: _passwordController,
+              decoration: InputDecoration(
+                  labelText: 'Meeting Password',
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter meeting password here...',
+                  contentPadding:
+                  EdgeInsets.symmetric(horizontal: 5.0, vertical: 2.0)),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
             RaisedButton(
-              padding: EdgeInsets.symmetric(vertical: 5.0,horizontal: 5.0),
-              child: Text('Start Meeting'),
+              child: Provider.of<UserDetails>(context, listen: false)
+                  .UserParticipantStatus ==
+                  "Teacher"?Text('Start Meeting'):Text('Join Meeting'),
               color: Colors.blue,
               textColor: Colors.white,
               shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(3.0)
-              ),
-              onPressed: (){
+                  borderRadius: BorderRadius.circular(3.0)),
+              onPressed: () async {
                 print('Start meeting pressed pressed');
+                await onJoin();
               },
             ),
-            SizedBox(height: 20.0,),
-            SizedBox(height: 30.0,child: Divider(color: Colors.black26,),width: 300.0),
-            SizedBox(height: 20.0,),
+            SizedBox(
+              height: 20.0,
+            ),
+            SizedBox(
+              height: 30.0,
+              child: Divider(
+                color: Colors.black26,
+              ),
+              width: 300.0,
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
             ListTile(
               leading: Image.asset('images/csv.png'),
               title: Text('Attendance Report'),
               subtitle: Text('Date:- 20/12/2010 Time:- 40:00 min'),
             )
-
           ],
         ),
       ),
     );
   }
+
+  Future<void> onJoin() async {
+    Token=_passwordController.text;
+    if (_channelController.text.isNotEmpty) {
+      // Wait for the permission for camera and microphone
+      // await _handleCameraAndMic(Permission.camera);
+      final perm = await html.window.navigator.permissions.query({"name": "camera"});
+      final perm2 = await html.window.navigator.permissions.query({"name": "microphone"});
+      // await _handleCameraAndMic(Permission.microphone);
+      //if(await Permission.camera.request().isGranted)
+      // Enter the page for live streaming and join channel using the channel name and role specified in the login page
+      if (Provider.of<UserDetails>(context, listen: false)
+          .UserParticipantStatus ==
+          "Teacher") {
+        _role = ClientRole.Broadcaster;
+      } else {
+        _role = ClientRole.Audience;
+      }
+      await Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => CallPage(
+            channelName: _channelController.text,
+            role: ClientRole.Broadcaster,
+          ),
+        ),
+      );
+      // await Navigator.push(
+      //   context,
+      //   MaterialPageRoute(
+      //     builder: (context) => CallPageSecond(
+      //       appId: 'f235a1310fcc4347af2e567d3a742a3c',
+      //       channel: _channelController.text,
+      //       video: true,
+      //       audio: true,
+      //       screen: true,
+      //       profile: '480p',
+      //       width: '1000',
+      //       height: '1000',
+      //       framerate: ' ',
+      //       codec: 'h264',
+      //       mode: 'live',
+      //     ),
+      //   ),
+      // );
+    }
+  }
+
+  // Ask for the permission for camera and microphone
+  // Future<void> _handleCameraAndMic(Permission permission) async {
+  //   final status = await permission.request();
+  //   print(status);
+  // }
 }
